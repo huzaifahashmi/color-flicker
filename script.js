33b350e65a9535adc328d7df83ef63b59f0b5e3f
Huzaifa Ahmed <72154213+huzaifahashmi@users.noreply.github.com>
2021-07-07 13:58:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let currentBackground = document.querySelector(".current-background");
+let changeColor = document.querySelector(".btn");
+let resetButton = document.querySelector(".reset");
+
+// Array containing colors
+
+let colors = [
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "gray",
+  "white",
+  "Orange",
+  "Violet",
+  "purple",
+  "pink",
+  "brown",
+];
+
+changeColor.addEventListener("click", function () {
+  let randomColor = Math.trunc(Math.random() * colors.length);
+  document.body.style.backgroundColor = `${colors[randomColor]}`;
+  currentBackground.textContent = colors[randomColor];
+});
+
+resetButton.addEventListener("click", function () {
+  document.body.style.backgroundColor = "rgb(204, 222, 255)";
+  currentBackground.textContent = "cornflowerblue";
+});
